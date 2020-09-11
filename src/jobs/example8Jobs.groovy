3fac4d89e13bca24c74406acb3cc65d54908533b
seman3 <Seman-3467!>
2020-09-11 12:02:39 +0700

    changed repo

--- src/jobs/example8Jobs.groovy
+++ src/jobs/example8Jobs.groovy
@@ -1,5 +1,5 @@
 String basePath = 'example8'
-String repo = 'sheehan/gradle-example'
+String repo = 'seman3/job-dsl-gradle-example'
 
 folder(basePath) {
     description 'This example shows how to use DSL extensions provided by other plugins.'
